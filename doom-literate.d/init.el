51bfd827f7b3f9f252c2f2fe5d3d27f0293f0a01
Aidan Scannell <scannell.aidan@gmail.com>
2022-08-20 22:28:36 +0300

    deleted old commented code

--- doom-literate.d/init.el
+++ doom-literate.d/init.el
@@ -145,14 +145,12 @@
        json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       ;; julia             ; a better, faster MATLAB
        (julia +lsp)             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;; latex             ; writing papers in Emacs has never been so fun
        (latex +latexmk
               +lsp
               +cdlatex)    ; writing papers in Emacs has never been so fun
-              ;; +present)    ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
